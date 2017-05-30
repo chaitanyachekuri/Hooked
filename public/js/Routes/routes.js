396bba8dca2c20a44d68fadfd8acdacc188d3728
cskcv <srichaitanya@skilleagle.com>
2017-05-30 09:26:33 -0400

    Folder structure in public is done

---
+++ public/js/Routes/routes.js
@@ -0,0 +1,10 @@
+app.config(function($routeProvider){
+
+    $routeProvider
+
+        .when('/', {
+            templateUrl: 'views/home.html',
+            controller: 'homeController'
+        });
+
+});
--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,11 @@
 /**
  * Created by cskcv on 5/23/2017.
  */
+var app = angular.module('hookedApp', ['ngRoute']);
+
+app.controller('hookedCtrl', ['$scope', function(scope){
+
+
+
+
+}]);
